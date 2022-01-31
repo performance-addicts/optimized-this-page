49e9079e14ffa128710f26d6570018837d0030a4
jhadev <joshappeldev@gmail.com>
2022-01-31 13:05:30 -0600

    test

--- assets/js/work.js
+++ assets/js/work.js
@@ -10,3 +10,11 @@ function fibonacci(num) {
 
   return fibonacci(num - 1) + fibonacci(num - 2);
 }
+
+window.addEventListener("load", () => {
+  const data2 = Array.from(Array(40).keys());
+  for (let i = 0; i < data.length; i++) {
+    const num = fibonacci(data[i]);
+    console.log(num);
+  }
+});
