a6edae2b8f0601e579ff6a5e0b1061b99588dcd3
jhadev <joshappeldev@gmail.com>
2022-01-31 14:00:39 -0600

    update

--- assets/js/work-final.js
+++ assets/js/work-final.js
@@ -11,18 +11,16 @@ function fibonacci(num) {
   return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  const data2 = Array.from(Array(40).keys());
-  for (let i = 0; i < data.length; i++) {
-    const num = fibonacci(data2[i]);
-    console.log(num);
-  }
-});
-
 window.addEventListener("load", () => {
   const data2 = Array.from(Array(50).keys());
   for (let i = 0; i < data.length; i++) {
     const num = fibonacci(data2[i]);
+    console.log(num);
+  }
+
+  const data3 = Array.from(Array(40).keys());
+  for (let i = 0; i < data.length; i++) {
+    const num = fibonacci(data3[i]);
     console.log(num);
   }
 });
